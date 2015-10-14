0195265bec73c40fcbbfbfb9ed1251f255d895c5
Dinh Han <dinh.han@sutrixmedia.com>
2015-10-14 17:35:21 +0700

    #feature-KBC-86: Create custom blocks and search form

---
+++ docroot/profiles/kandb/modules/custom/kandb_recherche/templates/recherche_minimal.tpl.php
@@ -0,0 +1,5 @@
+<h2>Recherche Minimal Form</h2>
+<?php
+  print render($form['search']);
+  print render($form['submit']);
+?>
---
+++ docroot/profiles/kandb/modules/custom/kandb_recherche/templates/recherche_simple.tpl.php
@@ -0,0 +1,7 @@
+<h2>Recherche Simple Form</h2>
+<?php
+  print render($form['search']);
+  print render($form['field_type']);
+  print render($form['field_prix']);
+  print render($form['submit']);
+?>
