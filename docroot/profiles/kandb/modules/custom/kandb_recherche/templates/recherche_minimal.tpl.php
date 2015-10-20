48bc9221e7c48afba189a5b18f62b7a6d9aba231
Dinh Han <dinh.han@sutrixmedia.com>
2015-10-20 15:53:22 +0700

    #feature-KBC-86: change input to button, fix miss test, change method from post to get

--- docroot/profiles/kandb/modules/custom/kandb_recherche/templates/recherche_minimal.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_recherche/templates/recherche_minimal.tpl.php
@@ -4,13 +4,13 @@ $errors = form_get_errors();
 <div class="searchFormular searchFormular-homepage">
     <form id="<?php print $form['#id']; ?>" data-abide="data-abide" class="searchFormular__form" novalidate="novalidate" accept-charset="UTF-8" method="<?php print $form['#method']; ?>" action="<?php print $form['#action']; ?>">
         <div class="input-withSubmit">
-            <label class="input"><span class="visually-hidden"></span>
+            <label class="input">
+                <span class="visually-hidden"><?php print t('Ville, département ou programme'); ?></span>
                 <?php print render($form['place']); ?>
-                <small class="error"><?php print $errors['place']; ?></small>
+                <small class="error"><?php print t('Veuillez saisir votre recherche'); ?></small>
             </label>
         </div>
 
         <?php print render($form['submit']); ?>
-        <?php print drupal_render_children($form); ?>
     </form>
 </div>
