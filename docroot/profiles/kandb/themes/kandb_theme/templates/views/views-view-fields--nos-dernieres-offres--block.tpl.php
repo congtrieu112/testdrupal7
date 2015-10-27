f741a3a35c08c37b0f5f71768dca970979f95ec4
hau.le <hau.le@sutrixmedia.com>
2015-10-27 17:19:12 +0700

    #KBC-126 update view field override template for offers block view

---
+++ docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-fields--nos-dernieres-offres--block.tpl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+$style = $row->field_field_image_principale[0]['rendered']['#image_style'];
+
+$ville_name = $row->field_field_programme_loc_ville[0]['rendered']['#title'];
+$departement_tax = $row->field_field_espace_vente_dpt[0]['rendered']['#options']['entity'];
+$departement_code = isset($departement_tax->field_numero_departement [LANGUAGE_NONE][0]['value']) ? $departement_tax->field_numero_departement [LANGUAGE_NONE][0]['value'] : '';
+?>
+<div class="slick-slider__item">
+    <article class="squaredImageItem squaredImageItem--stacked false">
+        <a href="<?php print url('node/' . $row->nid); ?>" title="<?php print $node->title; ?>" class="squaredImageItem__img">
+            <img src="<?php print image_style_url($style, $row->field_field_image_principale[0]['raw']['uri']); ?>" alt="<?php print $row->field_field_image_principale[0]['raw']['alt'] ?>"/>
+            <?php if ($row->field_promotion_programme_node_title): ?>
+                <div class="tag"><?php print t('Plus que deux T3 disponibles'); ?></div>
+                <div class="mention-legale hidden"><?php print $row->field_field_promotion_mention_legale[0]['rendered']['#markup']; ?></div>
+            <?php endif; ?>
+        </a>
+        <div class="squaredImageItem__infos">
+            <div class="squaredImageItem__details">
+                <a href="<?php print url('node/' . $row->nid); ?>" title="<?php print t('Go to programme page'); ?>" class="heading heading--small">
+                    <p class="heading__title">
+                        <?php print $ville_name . ' / ' . $departement_code; ?>
+                    </p>
+                    <p class="heading__title heading__title--sub"><?php print $row->node_title; ?></p>
+                </a>
+            </div>
+        </div>
+    </article>
+</div>
