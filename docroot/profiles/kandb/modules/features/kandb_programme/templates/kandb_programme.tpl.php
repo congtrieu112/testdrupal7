9a5879a9d876d82c606b33022ed0f4b3cad848e2
Dinh Han <dinh.han@sutrixmedia.com>
2015-11-03 17:21:34 +0700

    #feature-KBC-121-20151029: optimize features

--- docroot/profiles/kandb/modules/features/kandb_programme/templates/kandb_programme.tpl.php
+++ docroot/profiles/kandb/modules/features/kandb_programme/templates/kandb_programme.tpl.php
@@ -1,15 +1,11 @@
 <?php
-$path_img = kandb_theme_get_path('test_assets', 'kandb_theme');
-$logo_svg = kandb_theme_get_path('assets', 'kandb_theme') . '/images/logo-Kaufman-Broad.svg';
 
-
-$logementBlock = module_invoke('kandb_programme', 'block_view', 'logement_block');
-$documentBlock = module_invoke('kandb_programme', 'block_view', 'document_block');
-
-print render($documentBlock['content']);
-
-print render($logementBlock['content']['rs_price_max_min']);
-foreach($logementBlock['content']['rs_price_most'] as $key => $value) {
-  print render($logementBlock['content']['rs_price_most'][$key]);
-  print render($logementBlock['content']['rs_price_least'][$key]);
+print $content['rs_price_max_min'];
+foreach($content['rs_price_most'] as $key => $value) {
+  if(isset($content['rs_price_most'][$key])) {
+    print $content['rs_price_most'][$key];
+  }
+  if(isset($content['rs_price_least'][$key])) {
+    print $content['rs_price_least'][$key];
+  }
 }
