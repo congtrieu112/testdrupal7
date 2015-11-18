d71964984b576d9e051154766535d306d1ded77a
Grégory Montigny <gregory.montigny@digitaslbi.fr>
2015-11-18 16:47:31 +0100

    Update kandb_selection module + New kandb_settings module + Update Profile info and install

---
+++ docroot/profiles/kandb/modules/features/kandb_selection/templates/page_selection.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Selection Page which will load ajax
+ *
+ * receive 3 variables;
+ * $selection_programme_bien_avant_premiere
+ * $recherche
+ * $selection_article
+ */
+
+?>
+
+<?php
+  echo $selection_programme_bien_avant_premiere;
+  echo $recherche;
+  echo $selection_article;
+?>
