0ba34e5d34b516546d119c2bc983e3079dd3310f
Dinh Cong Trieu <ittrjeu@gmail.com>
2015-11-23 09:56:58 +0700

    #KB-415 implement the footer

--- docroot/profiles/kandb/themes/kandb_theme/templates/regions/region--footer.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/regions/region--footer.tpl.php
@@ -34,3 +34,88 @@
     <?php print $content; ?>
   </div>
 <?php endif; ?>
+<?php
+$path_img = kandb_theme_get_path('test_assets', 'kandb_theme');
+$logo_svg = kandb_theme_get_path('assets', 'kandb_theme') . '/images/logo-Kaufman-Broad.svg';
+global $base_url;
+$link_custom = $base_url;
+if(theme_get_setting('footer_link_custom')){
+  $link_custom = theme_get_setting('footer_link_custom');  
+}
+
+?>
+<footer class="siteFooter">
+    <div class="wrapper">
+        <div class="siteFooter__head"><a href="<?php print $base_url; ?>" title="retour à l'accueil" class="logo"><img src="<?php print $logo_svg;?>" alt="Kaufman&amp;Board"></a>
+            <div class="prescripteurs"><a href="<?php print $link_custom; ?>" class="btn-white">Espace prescripteur<span class="icon icon-arrow"></span></a></div>
+            <aside class="aside"><span class="sharing-label">suivez-nous sur</span>
+                <?php
+                $icon_setting = theme_get_setting('social_display');
+                if ($icon_setting) {
+                    $facebook = $youtube = $twiiter = "";
+                    if (theme_get_setting('footer_link_face')) {
+                        $facebook = theme_get_setting('footer_link_face');
+                    }
+                    if (theme_get_setting('footer_link_youtube')) {
+                        $youtube = theme_get_setting('footer_link_youtube');
+                    }
+                    if (theme_get_setting('footer_link_twiiter')) {
+                        $twiiter = theme_get_setting('footer_link_twiiter');
+                    }
+                    ?>
+                    <div class="sharing">
+                        <ul class="sharing__items">
+                            <li class="sharing__items__item"><a href="<?php print $facebook; ?>" title="partage sur Facebook" class="icon icon-facebook"></a></li>
+                            <li class="sharing__items__item"><a href="<?php print $twiiter; ?>#" title="partage sur Twitter" class="icon icon-twitter"></a></li>
+                            <li class="sharing__items__item"><a href="<?php print $youtube; ?>" title="partage sur Youtube" class="icon icon-youtube"></a></li>
+                        </ul>
+                    </div>
+                <?php } ?>
+            </aside>
+        </div>
+        <nav class="siteFooter__nav">
+            <?php
+            $menu_one = $menu_two = $menu_three = "";
+            if (!empty(menu_navigation_links('menu-footer-one')) && count(menu_navigation_links('menu-footer-one')) > 0) {
+                $menu_one = menu_navigation_links('menu-footer-one');
+                ?>
+                <ul class="show-for-medium-up searchItems">
+                <?php foreach ($menu_one as $item) { ?>
+                        <li><a href="<?php print $item['href']; ?>" title="<?php print $item['title'] ?>" class="textLink"><?php print $item['title'] ?></a></li>
+                    <?php } ?>
+
+                </ul>
+            <?php } ?>
+            <?php
+            if (!empty(menu_navigation_links('menu-footer-two')) && count(menu_navigation_links('menu-footer-two')) > 0) {
+                $menu_two = menu_navigation_links('menu-footer-two');
+                ?>
+                <ul >
+                    <?php foreach ($menu_tow as $item) { ?>
+                        <li><a href="<?php print $item['href']; ?>" title="<?php print $item['title'] ?>" class="textLink"><?php print $item['title'] ?></a></li>
+                    <?php } ?>
+
+                </ul>
+            <?php } ?>
+            <?php
+            if (!empty(menu_navigation_links('menu-footer-three')) && count(menu_navigation_links('menu-footer-three')) > 0) {
+                $menu_three = menu_navigation_links('menu-footer-three');
+                ?>
+                <ul >
+                    <?php foreach ($menu_three as $item) { ?>
+                        <li><a href="<?php print $item['href']; ?>" title="<?php print $item['title'] ?>" class="textLink"><?php print $item['title'] ?></a></li>
+                        <?php } ?>
+
+                </ul>
+            <?php } ?>
+        </nav>
+    </div>
+</footer>
+</div>
+
+
+
+
+
+
+
