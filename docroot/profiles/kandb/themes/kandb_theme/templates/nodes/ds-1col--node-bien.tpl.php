0bdd72a0e9876751c8c564fa381822af7e65f2e4
Grégory Montigny <gregory.montigny@digitaslbi.fr>
2015-11-24 11:36:18 +0100

    Bux Fix JS for search + Remove Popinleadform duplicate on node Bien + Update Asset / CSS / JS

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-bien.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-bien.tpl.php
@@ -221,11 +221,6 @@ if (isset($node->field_programme[LANGUAGE_NONE][0]['target_id'])) {
     <?php endif; ?>
 </section>
 <!-- [3rd party: visite-virtuelle] start-->
-<!-- [popinLeadForm popin] start-->
-<div id="popinLeadForm" data-reveal="data-reveal" aria-hidden="true" role="dialog" data-drupal-form="data-drupal-form" class="reveal-modal full scroll"></div>
-<!-- [popinLeadForm popin] end-->
-
-
 
 <!-- [More Available] start-->
 <?php
