ef273ca0e77dcfaa1cb57016890da71dfcc7682c
hau.le <hau.le@sutrixmedia.com>
2015-11-24 16:14:45 +0700

    #KBC-468 Integration Nouveaute page

---
+++ docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-unformatted--nouveaute--block.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php $_SESSION['nouveaute_promotion'] = 0; ?>
+<ul data-app-filter-target="prochainement" class="flex-content ul-unstyled">
+    <?php foreach ($rows as $id => $row): ?>
+      <?php print $row; ?>
+    <?php endforeach; ?>
+</ul>
