844e2baf35d9f156ab911999d91a5397bb9b4506
hau.le <hau.le@sutrixmedia.com>
2015-11-25 16:03:39 +0700

    #KBC-465 fix subtitle in homepage

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-homepage.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-homepage.tpl.php
@@ -19,7 +19,7 @@
 <section class="wrapper section-padding">
     <header class="heading heading--bordered">
         <h2 class="heading__title"><?php print render($content['field_hp_block_offer_titre']); ?></h2>
-        <p class="heading__title heading__title--sub"><?php print render($content['field_hp_block_offer_stitre']); ?></p>
+        <p class="heading__title heading__title--sub"><?php print $content['field_hp_block_offer_stitre'][0]['#markup']; ?></p>
     </header>
     <?php print render($content['hp_block_offre']); ?>
 <!--    <div class="btn-wrapper btn-wrapper--center"><a href="#" class="btn-rounded btn-primary">Voir toutes nos offres<span class="icon icon-arrow"></span></a>
@@ -32,7 +32,7 @@
     <div class="wrapper">
         <header class="heading heading--bordered">
             <h2 class="heading__title"><?php print render($content['field_hp_block_conseil_titre']); ?></h2>
-            <p class="heading__title heading__title--sub"><?php print render($content['field_hp_block_conseil_stitre']); ?></p>
+            <p class="heading__title heading__title--sub"><?php print $content['field_hp_block_conseil_stitre'][0]['#markup']; ?></p>
         </header>
         <div class="homeDocs__main">
             <!-- images need to have 2 formats in data-interchange attribute:
