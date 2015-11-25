3337faf4426ee9e4c58dcc3aba276e1a43c9e272
Grégory Montigny <gregory.montigny@digitaslbi.fr>
2015-11-25 16:40:28 +0100

    Update Webforms + Theme

--- docroot/profiles/kandb/themes/kandb_theme/templates/pages/page--ajax.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/pages/page--ajax.tpl.php
@@ -1,1 +1,6 @@
+<?php if ($title): ?>
+  <div class="heading heading--bordered heading--small">
+    <div class="heading__title"><?php print $title; ?></div>
+  </div>
+<?php endif; ?>
 <?php print render($page['content']); ?>
