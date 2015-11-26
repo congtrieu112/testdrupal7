a8c7c3182693b8986b33ddb169ea0afbedae73c2
Nguyen Dinh Tu <tu.nguyen@php-tunguyen.sutrixmedia1.com>
2015-11-26 11:56:16 +0700

    feature-KBC-449-450-451-452-453-454: Fix bug contact us

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/node--webform--ajax.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/node--webform--ajax.tpl.php
@@ -1,11 +1,7 @@
 <?php
 $webform_title = $node->title;
 if ($node->webform['machine_name'] == '_tre_rappel_') {
-  foreach ($node->webform['machine_name']['components'] as $components) {
-    if ($components['machine_name'] == '_tre_rappel___popin_title') {
-      $webform_title = $components['value'];
-    }
-  }
+  $webform_title = t('Rappelez moi');
 }
 ?>
 <div class="heading heading--bordered heading--small">
