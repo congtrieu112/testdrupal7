1c657b0ba7914ecd72e2f11fb9376b9e46479be1
Pierre-Louis Launay <pierre-louis.launay@digitaslbi.fr>
2015-11-30 18:23:41 +0100

    Improve the JS

--- docroot/profiles/kandb/modules/features/kandb_partenaire/js/kandb_partenaire.js
+++ docroot/profiles/kandb/modules/features/kandb_partenaire/js/kandb_partenaire.js
@@ -3,8 +3,12 @@
     attach: function (context, settings) {
         inputSelectAllPrograms = $('#edit-field-partenaire-programmes');
         inputRadioAllPrograms = $('input[type=radio][name="field_partenaire_export[und]"]:last');
-
         inputSelectAllPrograms.hide();
+
+        if (inputRadioAllPrograms.attr('checked')) {
+            inputSelectAllPrograms.show();
+        }
+
         $('input[type=radio][name="field_partenaire_export[und]"').change(function(){
             if($(this).attr("id") === inputRadioAllPrograms.attr("id")){
                 inputSelectAllPrograms.show();
