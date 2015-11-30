5fb93a03542c2699295bb730c26f2f55952aebff
NICOLAS CORDONNIER <nicolas.cordonnier@digitaslbi.fr>
2015-11-30 17:44:32 +0100

    task-KBC-512 - JS file to hide select program when the last input radio for programs is checked

--- docroot/profiles/kandb/modules/features/kandb_partenaire/js/kandb_partenaire.js
+++ docroot/profiles/kandb/modules/features/kandb_partenaire/js/kandb_partenaire.js
@@ -1,1 +1,21 @@
-alert('Hello World!');
+// $('#edit-field-partenaire-programmes').hide();
+// $('input[type=radio]#edit-field-partenaire-export-und-3').on( "click", function() {
+//     $('#edit-field-partenaire-programmes').show();
+// });
+(function ($) {
+  Drupal.behaviors.yourBehaviorName = {
+    attach: function (context, settings) {
+        inputSelectAllPrograms = $('#edit-field-partenaire-programmes');
+        inputRadioAllPrograms = $('input[type=radio][name="field_partenaire_export[und]"]:last');
+
+        inputSelectAllPrograms.hide();
+        $('input[type=radio][name="field_partenaire_export[und]"').change(function(){
+            if($(this).attr("id") === inputRadioAllPrograms.attr("id")){
+                inputSelectAllPrograms.show();
+            } else {
+                inputSelectAllPrograms.hide();
+            }
+        });
+    }
+  };
+})(jQuery);
