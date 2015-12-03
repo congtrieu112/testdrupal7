75643c902303af3705ae55224c1a0805fe49d056
hau.le <hau.le@sutrixmedia.com>
2015-12-03 13:49:54 +0700

    #KBC-590 change tab title

--- docroot/profiles/kandb/themes/kandb_theme/templates/blocks/program_characteristic.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/blocks/program_characteristic.tpl.php
@@ -1,30 +1,29 @@
 <?php
-//var_dump(variable_get('image_default_slider_exterieur_image'));die;
 if (!isset($programme_variables) || empty($programme_variables)) {
   return;
 }
 $slide0 = $slide1 = $slide2 = $slide3 = FALSE;
-if((isset($programme_variables['slider_exterieur_image_small']) 
-    && isset($programme_variables['slider_exterieur_image_medium'])) 
-    || $programme_variables['slider_exterieur_description'] != '' 
+if((isset($programme_variables['slider_exterieur_image_small'])
+    && isset($programme_variables['slider_exterieur_image_medium']))
+    || $programme_variables['slider_exterieur_description'] != ''
     || $programme_variables['slider_exterieur_titre'] != '' ) {
   $slide0 = TRUE;
 }
-if((isset($programme_variables['slider_interieur_image_small']) 
-    && isset($programme_variables['slider_interieur_image_medium'])) 
-    || $programme_variables['slider_interieur_description'] != '' 
+if((isset($programme_variables['slider_interieur_image_small'])
+    && isset($programme_variables['slider_interieur_image_medium']))
+    || $programme_variables['slider_interieur_description'] != ''
     || $programme_variables['slider_interieur_titre'] != '' ) {
   $slide1 = TRUE;
 }
-if((isset($programme_variables['slider_securite_image_small']) 
-    && isset($programme_variables['slider_securite_image_medium'])) 
-    || $programme_variables['slider_securite_description'] != '' 
+if((isset($programme_variables['slider_securite_image_small'])
+    && isset($programme_variables['slider_securite_image_medium']))
+    || $programme_variables['slider_securite_description'] != ''
     || $programme_variables['slider_securite_titre'] != '' ) {
   $slide2 = TRUE;
 }
-if((isset($programme_variables['slider_rt2012_image_small']) 
-    && isset($programme_variables['slider_rt2012_image_medium'])) 
-    || $programme_variables['slider_rt2012_description'] != '' 
+if((isset($programme_variables['slider_rt2012_image_small'])
+    && isset($programme_variables['slider_rt2012_image_medium']))
+    || $programme_variables['slider_rt2012_description'] != ''
     || $programme_variables['slider_rt2012_titre'] != '' ) {
   $slide3 = TRUE;
 }
@@ -47,7 +46,7 @@ if((isset($programme_variables['slider_rt2012_image_small'])
       <li><a href="#slide1" data-slick-links="data-slick-links" role="tab" aria-controls="slide1" aria-selected="false"><?php print t('Intérieur'); ?></a></li>
       <?php endif; ?>
       <?php if($slide2) : ?>
-      <li><a href="#slide2" data-slick-links="data-slick-links" role="tab" aria-controls="slide2" aria-selected="false"><?php print t('Services'); ?></a></li>
+      <li><a href="#slide2" data-slick-links="data-slick-links" role="tab" aria-controls="slide2" aria-selected="false"><?php print t('Sécurité'); ?></a></li>
       <?php endif; ?>
       <?php if($slide3) : ?>
       <li><a href="#slide3" data-slick-links="data-slick-links" role="tab" aria-controls="slide03" aria-selected="false"><?php print t('RT 2012'); ?></a></li>
@@ -61,12 +60,12 @@ if((isset($programme_variables['slider_rt2012_image_small'])
           $alt = isset($programme_variables['slider_exterieur_image_alt']) ? $programme_variables['slider_exterieur_image_alt'] : '';
           $small = isset($programme_variables['slider_exterieur_image_small']) ? $programme_variables['slider_exterieur_image_small'] : '';
           $medium = isset($programme_variables['slider_exterieur_image_medium']) ? $programme_variables['slider_exterieur_image_medium'] : '';
-          
+
           $default_image = variable_get('image_default_slider_exterieur_image');
           if(empty($small) && !empty($default_image)){
             $small = image_style_url("program_characteristic_small", $default_image);
           }
-          
+
           if(empty($medium) && !empty($default_image)){
             $medium = image_style_url("program_characteristic_medium", $default_image);
           }
@@ -92,12 +91,12 @@ if((isset($programme_variables['slider_rt2012_image_small'])
           $alt = isset($programme_variables['slider_interieur_image_alt']) ? $programme_variables['slider_interieur_image_alt'] : '';
           $small = isset($programme_variables['slider_interieur_image_small']) ? $programme_variables['slider_interieur_image_small'] : '';
           $medium = isset($programme_variables['slider_interieur_image_medium']) ? $programme_variables['slider_interieur_image_medium'] : '';
-          
+
           $default_image = variable_get('image_default_slider_interieur_image');
           if(empty($small) && !empty($default_image)){
             $small = image_style_url("program_characteristic_small", $default_image);
           }
-          
+
           if(empty($medium) && !empty($default_image)){
             $medium = image_style_url("program_characteristic_medium", $default_image);
           }
@@ -118,23 +117,23 @@ if((isset($programme_variables['slider_rt2012_image_small'])
       <?php endif; ?>
       <?php if($slide2) : ?>
       <li id="slide2">
-        <a href="#slide2" data-app-accordion-link="data-app-accordion-link" role="tab" class="show-for-small-only accordion__link"><?php print t('Services') ;?><span class="display-status"></span></a>
+        <a href="#slide2" data-app-accordion-link="data-app-accordion-link" role="tab" class="show-for-small-only accordion__link"><?php print t('Sécurité') ;?><span class="display-status"></span></a>
         <?php
           $alt = isset($programme_variables['slider_securite_image_alt']) ? $programme_variables['slider_securite_image_alt'] : '';
           $small = isset($programme_variables['slider_securite_image_small']) ? $programme_variables['slider_securite_image_small'] : '';
           $medium = isset($programme_variables['slider_securite_image_medium']) ? $programme_variables['slider_securite_image_medium'] : '';
-          
+
           $default_image = variable_get('image_default_slider_securite_image');
           if(empty($small) && !empty($default_image)){
             $small = image_style_url("program_characteristic_small", $default_image);
           }
-          
+
           if(empty($medium) && !empty($default_image)){
             $medium = image_style_url("program_characteristic_medium", $default_image);
           }
         ?>
         <article data-app-accordion-content="data-app-accordion-content" class="programCharacteristicsItem">
-          <?php if(!empty($small) || !empty($medium)){?>  
+          <?php if(!empty($small) || !empty($medium)){?>
             <div class="programCharacteristicsItem__img">
               <img alt="<?php print $alt ;?>" data-interchange="[<?php print $small ;?>, (small)], [<?php print $medium ;?>, (medium)]">
               <noscript>&lt;img src="<?php print $medium ;?>" alt="<?php print $alt; ?>"/&gt;</noscript>
@@ -154,12 +153,12 @@ if((isset($programme_variables['slider_rt2012_image_small'])
           $alt = isset($programme_variables['slider_rt2012_image_alt']) ? $programme_variables['slider_rt2012_image_alt'] : '';
           $small = isset($programme_variables['slider_rt2012_image_small']) ? $programme_variables['slider_rt2012_image_small'] : '';
           $medium = isset($programme_variables['slider_rt2012_image_medium']) ? $programme_variables['slider_rt2012_image_medium'] : '';
-          
+
           $default_image = variable_get('image_default_slider_rt2012_image');
           if(empty($small) && !empty($default_image)){
             $small = image_style_url("program_characteristic_small", $default_image);
           }
-          
+
           if(empty($medium) && !empty($default_image)){
             $medium = image_style_url("program_characteristic_medium", $default_image);
           }
