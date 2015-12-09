0a842f9c30bb27ccb3bf46db7697075c68c39681
Gregory <gregory.montigny@digitaslbi.fr>
2015-12-09 11:44:13 +0100

    Update Selection Module

--- docroot/profiles/kandb/modules/features/kandb_selection/templates/page_selection.tpl.php
+++ docroot/profiles/kandb/modules/features/kandb_selection/templates/page_selection.tpl.php
@@ -2,17 +2,17 @@
 /**
  * @file
  * Selection Page which will load ajax
- *
- * receive 3 variables;
- * $selection_programme_bien_avant_premiere
- * $recherche
- * $selection_article
  */
 
 ?>
-
-<?php
-  echo $selection_programme_bien_avant_premiere;
-  echo $recherche;
-  echo $selection_article;
-?>
+<section data-interchange="[<?php print $backgrounds['small']; ?>, (small)], [<?php print $backgrounds['medium']; ?>, (medium)]" class="narrow-header" data-uuid="interchange-ihxhe4i33" style="background-image: url(&quot;<?php print $backgrounds['medium']; ?>&quot;);">
+  <div class="wrapper">
+    <div class="heading heading--bordered heading--white">
+      <div class="heading__title"><?php print $title; ?></div>
+      <div class="heading__title heading__title--sub"><?php print $sub_title; ?></div>
+    </div>
+  </div>
+</section>
+<div data-app-ajax="cookies" data-app-ajax-url="<?php print $ajax_url; ?>" class="">
+</div>
+<?php print $contact; ?>
