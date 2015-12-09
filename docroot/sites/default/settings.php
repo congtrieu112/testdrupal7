12589d1225e96ee193d164e2927e3fc2540b19c3
Gregory <gregory.montigny@digitaslbi.fr>
2015-12-09 15:26:12 +0100

    Update Settings for Acquia Purge and Domain compatibility

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -588,10 +588,33 @@ if ( (strpos($_GET['q'], 'admin') === 0) ||
   (strpos($_GET['q'], 'import') === 0)) {
   ini_set('memory_limit', '512M');
 }
-
 // Include Acquia Drupal settings
 if (file_exists('/var/www/site-php')) {
     require '/var/www/site-php/kaufmanetbroad/kaufmanetbroad-settings.inc';
+}
+// Domain for Acquia Purge
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
+    case 'dev':
+      $sites['dev.kbpatrimoine.com'] = 'dev.kbpatrimoine.com';
+      $sites['dev.kaufmanbroad.fr'] = 'dev.kaufmanbroad.fr';
+      $sites['kaufmanetbroaddev.prod.acquia-sites.com'] = 'kaufmanetbroaddev.prod.acquia-sites.com';
+      break;
+
+    case 'test':
+      $sites['stage.kbpatrimoine.com'] = 'stage.kbpatrimoine.com';
+      $sites['stage.kaufmanbroad.fr'] = 'stage.kaufmanbroad.fr';
+      $sites['kaufmanetbroadstg.prod.acquia-sites.com'] = 'kaufmanetbroadstg.prod.acquia-sites.com';
+      break;
+
+    case 'prod':
+      $sites['www.kbpatrimoine.com'] = 'www.kbpatrimoine.com';
+      $sites['www.kaufmanbroad.fr'] = 'www.kaufmanbroad.fr';
+      $sites['prod.kbpatrimoine.com'] = 'prod.kbpatrimoine.com';
+      $sites['prod.kaufmanbroad.fr'] = 'prod.kaufmanbroad.fr';
+      $sites['kaufmanetbroad.prod.acquia-sites.com'] = 'kaufmanetbroad.prod.acquia-sites.com';
+      break;
+  }
 }
 
 /**
