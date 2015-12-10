03ffd7ed75a18d8cfd4382744e0d23f9fc42f58d
Gregory <gregory.montigny@digitaslbi.fr>
2015-12-10 15:57:47 +0100

    Move the sites definition in sites.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -592,30 +592,6 @@ if ( (strpos($_GET['q'], 'admin') === 0) ||
 if (file_exists('/var/www/site-php')) {
     require '/var/www/site-php/kaufmanetbroad/kaufmanetbroad-settings.inc';
 }
-// Domain for Acquia Purge
-if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
-    case 'dev':
-      $sites['dev.kbpatrimoine.com'] = 'dev.kbpatrimoine.com';
-      $sites['dev.kaufmanbroad.fr'] = 'dev.kaufmanbroad.fr';
-      $sites['kaufmanetbroaddev.prod.acquia-sites.com'] = 'kaufmanetbroaddev.prod.acquia-sites.com';
-      break;
-
-    case 'test':
-      $sites['stage.kbpatrimoine.com'] = 'stage.kbpatrimoine.com';
-      $sites['stage.kaufmanbroad.fr'] = 'stage.kaufmanbroad.fr';
-      $sites['kaufmanetbroadstg.prod.acquia-sites.com'] = 'kaufmanetbroadstg.prod.acquia-sites.com';
-      break;
-
-    case 'prod':
-      $sites['www.kbpatrimoine.com'] = 'www.kbpatrimoine.com';
-      $sites['www.kaufmanbroad.fr'] = 'www.kaufmanbroad.fr';
-      $sites['prod.kbpatrimoine.com'] = 'prod.kbpatrimoine.com';
-      $sites['prod.kaufmanbroad.fr'] = 'prod.kaufmanbroad.fr';
-      $sites['kaufmanetbroad.prod.acquia-sites.com'] = 'kaufmanetbroad.prod.acquia-sites.com';
-      break;
-  }
-}
 
 /**
  * Add the domain module setup routine.
