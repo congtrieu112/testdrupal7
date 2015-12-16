c655593b2794bb28dc4cd83d9f950455bfc1a157
tu.tran <tu.tran@sutrix.media.com>
2015-12-16 15:11:42 +0700

    #KBC-921 Pop-up is displayed although that promotion does not have Mention legal

--- docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-fields--avant-premiere-check--block.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-fields--avant-premiere-check--block.tpl.php
@@ -69,16 +69,18 @@ if (isset($view->promotion_duplicate) && count($view->promotion_duplicate)) {
                 <?php if ($row->field_promotion_avant_premiere_node_title && $available && $status_promotion && $_SESSION['avant_promotion'] < 3): ?>
                   <li>
                       <button data-reveal-trigger="avant_premiere_<?php print $row->field_promotion_avant_premiere_node_nid; ?>" class="tag tag--important"><?php print $row->field_promotion_avant_premiere_node_title; ?></button>
-                      <!-- [popin] start-->
-                      <div data-reveal="avant_premiere_<?php print $row->field_promotion_avant_premiere_node_nid; ?>" aria-hidden="true" role="dialog" class="reveal-modal full scroll reduced">
-                          <div class="reveal-modal__wrapper"><a aria-label="Fermer" class="close-reveal-modal icon icon-close"></a>
-                              <p class="heading heading--bordered heading--small"><strong class="heading__title"><?php print $row->field_promotion_avant_premiere_node_title; ?></strong></p>
-                              <p><?php print $row->field_field_promotion_mention_legale[0]['rendered']['#markup']; ?></p>
-                          </div>
-                      </div>
-                      <!-- [popin] end-->
+                      <?php if($row->field_field_promotion_mention_legale[0]['rendered']['#markup']) : ?>
+                        <!-- [popin] start-->
+                        <div data-reveal="avant_premiere_<?php print $row->field_promotion_avant_premiere_node_nid; ?>" aria-hidden="true" role="dialog" class="reveal-modal full scroll reduced">
+                            <div class="reveal-modal__wrapper"><a aria-label="Fermer" class="close-reveal-modal icon icon-close"></a>
+                                <p class="heading heading--bordered heading--small"><strong class="heading__title"><?php print $row->field_promotion_avant_premiere_node_title; ?></strong></p>
+                                <p><?php print $row->field_field_promotion_mention_legale[0]['rendered']['#markup']; ?></p>
+                            </div>
+                        </div>
+                        <!-- [popin] end-->
+                      <?php endif; ?>
                   </li>
-                  
+
                   <?php
                   if ($promotion) :
                     foreach ($promotion as $value) :
