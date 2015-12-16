74bae7f5883db1bd304adb4af692946ec9dcf250
hau.le <hau.le@sutrixmedia.com>
2015-12-16 15:35:06 +0700

    #KBC-925 crate theme for groupe home page

---
+++ docroot/profiles/kandb/modules/custom/kandb_group/templates/group_homepage.tpl.php
@@ -0,0 +1,66 @@
+<?php
+if (isset($data['group_header'])):
+  print render($data['group_header']);
+endif;
+?>
+<section class="section-padding groupHomepageArticle bg-lightGrey">
+    <div class="wrapper">
+        <div data-equalizer data-equalizer-mq="medium-up" class="articleList__list">
+            <div data-equalizer-watch class="articleList__item">
+                <div class="inner">
+                    <div class="articleList__item__infos">
+                        <h4 class="articleList__item__infos__heading">Derniers Communiqués</h4>
+                        <dl class="articleList__item__infos__list">
+                            <dt><a href="#"><span class="icon icon-communique"></span><span class="text">20.06.2015</span></a></dt>
+                            <dd>
+                                <p>Cras mattis consectetur purus sit amet fermentum</p>
+                            </dd>
+                            <dt><a href="#"><span class="icon icon-communique"></span><span class="text">03.02.2015</span></a></dt>
+                            <dd>
+                                <p>Cras mattis consectetur purus sit amet fermentum</p>
+                            </dd>
+                            <dt><a href="#"><span class="icon icon-communique"></span><span class="text">15.01.2015</span></a></dt>
+                            <dd>
+                                <p>Cras mattis consectetur purus sit amet fermentum</p>
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+            <div data-equalizer-watch="data-equalizer-watch" class="articleList__item">
+                <div class="inner">
+                    <div class="articleList__item__img">
+                        <!-- [Responsive img] start--><img alt="test" data-interchange="[test_assets/advices-small.jpg, (small)], [test_assets/advices-medium.jpg, (large)]"/>
+                        <noscript><img src="test_assets/advices-medium.jpg" alt="test"/></noscript>
+                        <!-- [Responsive img] end-->
+                    </div>
+                    <div class="articleList__item__infos">
+                        <h4 class="articleList__item__infos__heading">Qualite: Nos labels</h4>
+                        <ul class="articleList__item__infos__links">
+                            <li><a href="#" title="La garatie de remboursement ou d'achevement">La garatie de remboursement ou d'achevement</a></li>
+                            <li><a href="#" title="La garatie de remboursement ou">La garatie de remboursement ou</a></li>
+                            <li><a href="#" title="La garatie de remboursement ou d'achevement remboursement ou d'achevement">La garatie de remboursement ou d'achevement remboursement ou d'achevement</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div data-equalizer-watch="data-equalizer-watch" class="articleList__item">
+                <div class="inner">
+                    <div class="articleList__item__img">
+                        <!-- [Responsive img] start--><img alt="test" data-interchange="[test_assets/advices-small.jpg, (small)], [test_assets/advices-medium.jpg, (large)]"/>
+                        <noscript><img src="test_assets/advices-medium.jpg" alt="test"/></noscript>
+                        <!-- [Responsive img] end-->
+                    </div>
+                    <div class="articleList__item__infos">
+                        <h4 class="articleList__item__infos__heading">Frais réduit et taux preferentiels</h4>
+                        <ul class="articleList__item__infos__links">
+                            <li><a href="#" title="La garatie de remboursement ou d'achevement">La garatie de remboursement ou d'achevement</a></li>
+                            <li><a href="#" title="La garatie de remboursement ou">La garatie de remboursement ou</a></li>
+                            <li><a href="#" title="La garatie de">La garatie de</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
