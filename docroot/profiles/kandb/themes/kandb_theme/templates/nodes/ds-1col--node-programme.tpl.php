9d736499baf16436e6f1a61befca73d14720ed5f
Trang.Tran <trang.tran@sutrixmedia.com>
2015-12-17 14:15:54 +0700

    Program name, city and tags are missing

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-programme.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-programme.tpl.php
@@ -1,6 +1,50 @@
 <!-- [programHeader] start-->
 <header class="programHeader">
     <!-- mobile heading-->
+    <div class="wrapper show-for-small-only">
+      <h1 class="heading heading--bordered">
+        <?php if ($program_loc_ville) : ?>
+          <div class="heading__title">
+            <?php print $program_loc_ville; ?>
+            <?php
+            if ($programme_loc_arr_name) :
+              print $programme_loc_arr_name;
+            elseif ($program_loc_department) :
+              print $program_loc_department;
+            endif;
+            ?>
+          </div>
+        <?php endif; ?>
+        <?php if ($title) : ?>
+          <div class="heading__title heading__title--sub"><?php print $title; ?></div>
+        <?php endif; ?>
+      </h1>
+      <ul class="tags-list">
+        <?php if ($nouveau) : ?>
+          <li>
+            <div class="tag tag--important"><?php print t('Nouveauté'); ?></div>
+          </li>
+        <?php endif; ?>
+        <?php
+        if ($promotions) :
+          foreach ($promotions as $promotion) :
+            $triger_promotion = 'promotion-' . $promotion->nid;
+            ?>
+            <li>
+              <button class="tag tag--important" data-reveal-trigger="<?php print isset($promotion->field_promotion_mention_legale[LANGUAGE_NONE][0]['value']) ? $triger_promotion : ''; ?>" class="tag" tabindex="0"><?php print $promotion->title; ?></button>
+              <div data-reveal="<?php print $triger_promotion; ?>" aria-hidden="true" role="dialog" class="reveal-modal full scroll reduced">
+                <div class="reveal-modal__wrapper"><a aria-label="Fermer" class="close-reveal-modal icon icon-close"></a>
+                  <p class="heading heading--bordered heading--small"><strong class="heading__title"><?php print $promotion->title; ?></strong></p>
+                  <p><?php print isset($promotion->field_promotion_mention_legale[LANGUAGE_NONE][0]['value']) ? $promotion->field_promotion_mention_legale[LANGUAGE_NONE][0]['value'] : ''; ?></p>
+                </div>
+              </div>
+              <!-- [popin] end-->
+            </li>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      </ul>
+    </div>
+
     <div class="programHeader__figure">
         <!-- images need to have 2 formats see data-exchange attribute:
         - small: 640 x 316 (heavy compression)
