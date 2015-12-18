d899eb0a9dc2debd46b28c313c280a914f81c797
Gregory <gregory.montigny@digitaslbi.fr>
2015-12-18 10:23:34 +0100

    Update

---
+++ docroot/profiles/kandb/modules/features/kandb_seo/templates/block_letter.tpl.php
@@ -0,0 +1,11 @@
+<div id="block_letter" >
+  <?php if(!empty($location)) : ?>
+    <ul>
+      <?php foreach ($location as $text => $url) : ?>
+        <li>
+          <a href="/<?php print $url; ?>" class="<?php print ($url == $selected ? 'active' : ''); ?>" ><?php print $text; ?></a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+</div>
