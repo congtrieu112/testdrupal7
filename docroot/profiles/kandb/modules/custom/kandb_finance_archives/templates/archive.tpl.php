e3c8cf0725cfca8cbf4ffd33761a38fc2ba75f78
Luu Quoc Trung <trung.lq@php-trunglq.sutrixmedia1.com>
2016-01-06 18:29:57 +0700

    feature-KBC-1169

--- docroot/profiles/kandb/modules/custom/kandb_finance_archives/templates/archive.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_finance_archives/templates/archive.tpl.php
@@ -2,16 +2,26 @@
 if (!isset($content_archives) || empty($content_archives)) {
   return;
 }
+
 $class_first_block = 'active';
+
+$title = '';
+if(array_key_exists(arg(3), $content_archives['tab_header'])):
+    $data = $content_archives['tab_header'][arg(3)];
+    $title = $data['tab_title'];
+else :
+    $title =  t('Les actualités');
+endif;
+
 print theme('finance_header_block');
 ?>
 <section class="section-padding">
   <div class="wrapper">
     <header class="heading heading--bordered">
-      <h1 class="heading__title"><?php print $page_title; ?></h1>
+      <h1 class="heading__title"><?php print t('Documents à télécharger'); ?></h1>
     </header>
     <nav class="form-dropdown form-dropdown--responsive">
-      <button aria-expanded="false" aria-controls="dropdown-downloadDocs" data-app-dropdown data-app-dropdown-responsive="small-only" class="form-dropdown__trigger"><?php print t('Les actualités'); ?><span aria-hidden="true" class="icon icon-expand"></span></button>
+      <button aria-expanded="false" aria-controls="dropdown-downloadDocs" data-app-dropdown data-app-dropdown-responsive="small-only" class="form-dropdown__trigger"><?php print $title; ?><span aria-hidden="true" class="icon icon-expand"></span></button>
       <div id="dropdown-downloadDocs" aria-hidden="true" class="form-dropdown__content hidden">
         <ul class="ul-unstyled undo-padding">
           <?php foreach($content_archives['tab_header'] as $tab_content) : ?>
