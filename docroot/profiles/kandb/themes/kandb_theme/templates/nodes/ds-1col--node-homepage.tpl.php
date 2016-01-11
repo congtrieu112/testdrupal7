2062e1a0f34705e47a5e8f437b3a883afc49f67f
hau.le <hau.le@sutrixmedia.com>
2016-01-11 16:57:30 +0700

    #KBC-1257 create file zip and show link download

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-homepage.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-homepage.tpl.php
@@ -70,11 +70,41 @@ $bien_total = get_total_bien_by_status_site();
     </div>
 </section>
 <!-- [homeDocs] end-->
+<?php
+$number_pdf = count($node->field_hp_block_document['und']);
+?>
 <!-- [references] start-->
 <section class="wrapper section-padding">
     <header class="heading heading--bordered">
         <h2 class="heading__title"><?php print render($content['field_hp_block_ref_titre']); ?></h2>
         <p class="heading__title heading__title--sub"><?php print render($content['field_hp_block_ref_stitre'][0]['#markup']); ?></p>
+        <?php
+        if ($number_pdf > 1):
+          $addMore = '_';
+          $nid = $node->nid;
+          $path = file_create_url('public://');
+          $real_path = drupal_realpath('public://');
+          $fileName = 'Habitat' . $addMore . preg_replace('@[^a-z0-9-]+@', '-', strtolower($node->title)) . '.zip';
+          if (file_exists($real_path . '/Habitat/archive/' . $nid . '/')) :
+            $filePath = $real_path . '/Habitat/archive/' . $nid . '/' . $fileName;
+            $linkfile = $path . 'Habitat/archive/' . $nid . '/' . $fileName;
+            if ($filePath) :
+              if (file_exists($filePath)) :
+                ?>
+                <div class="downloadDocs__item__link">
+                    <a href="<?php print $linkfile; ?>" title="<?php print $fileName; ?>"><span class="icon icon-download-pdf"></span></a>
+                </div>
+                <?php
+              endif;
+            endif;
+          endif;
+          ?>
+        <?php elseif ($number_pdf > 0):
+          ?>
+          <div class="downloadDocs__item__link">
+              <a href="<?php print url('download-document-file/' . $node->field_hp_block_document['und'][0]['fid']) ?>" title="<?php print $node->field_hp_block_document['und'][0]['filename']; ?>"><span class="icon icon-download-pdf"></span></a>
+          </div>
+        <?php endif; ?>
     </header>
     <?php print render($content['habitat_carousel']); ?>
     <div class="btn-wrapper btn-wrapper--center">
