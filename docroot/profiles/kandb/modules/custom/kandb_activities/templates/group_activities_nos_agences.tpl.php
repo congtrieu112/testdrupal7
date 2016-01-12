b80e4ed1ee0db4692195980ba075071959c716db
Trang.Tran <trang.tran@sutrixmedia.com>
2016-01-12 11:25:14 +0700

    Feature-KBC-1263 The drop down list should reduce the width

--- docroot/profiles/kandb/modules/custom/kandb_activities/templates/group_activities_nos_agences.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_activities/templates/group_activities_nos_agences.tpl.php
@@ -9,16 +9,18 @@ $arg = arg();
             <p class="activities__desc"><?php print variable_get('desciption_group_agences_section', t('Retrouvez ci-dessous les coordonées de l\'ensemble des conseillers régionaux K&B Patrimoine')); ?></p>
         </h2>
     </div>
-    <nav class="form-dropdown form-dropdown--responsive">
-        <button class="form-dropdown__trigger" data-app-dropdown-responsive="small-only" data-app-dropdown="data-app-dropdown" aria-controls="dropdown-downloadDocs" aria-expanded="false">Les actualités<span class="icon icon-expand" aria-hidden="true"></span></button>
-        <div class="form-dropdown__content hidden" aria-hidden="true" id="dropdown-downloadDocs">
-            <ul class="ul-unstyled undo-padding">
-                <li class="bordered"><a class="<?php print $arg[2] == 'nos-agences' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-agences'); ?>"><span><?php print t('Nos agences'); ?></span></a></li>
-                <li class="bordered"><a class="<?php print $arg[2] == 'nos-services' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-services'); ?>"><span><?php print t('Nos services'); ?></span></a></li>
-                <li class="bordered"><a class="<?php print $arg[2] == 'nos-showroom' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-showroom'); ?>"><span><?php print t('Nos showroom'); ?></span></a></li>
-            </ul>
-        </div>
-    </nav>
+    <div class="wrapper">
+        <nav class="form-dropdown form-dropdown--responsive">
+            <button class="form-dropdown__trigger" data-app-dropdown-responsive="small-only" data-app-dropdown="data-app-dropdown" aria-controls="dropdown-downloadDocs" aria-expanded="false">Les actualités<span class="icon icon-expand" aria-hidden="true"></span></button>
+            <div class="form-dropdown__content hidden" aria-hidden="true" id="dropdown-downloadDocs">
+                <ul class="ul-unstyled undo-padding">
+                    <li class="bordered"><a class="<?php print $arg[2] == 'nos-agences' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-agences'); ?>"><span><?php print t('Nos agences'); ?></span></a></li>
+                    <li class="bordered"><a class="<?php print $arg[2] == 'nos-services' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-services'); ?>"><span><?php print t('Nos services'); ?></span></a></li>
+                    <li class="bordered"><a class="<?php print $arg[2] == 'nos-showroom' ? 'active' : ''; ?>" href="<?php print url('corporate/activites/nos-showroom'); ?>"><span><?php print t('Nos showroom'); ?></span></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
     <?php
     if ($region_contents) : ?>
       <div class="wrapper contacts">
