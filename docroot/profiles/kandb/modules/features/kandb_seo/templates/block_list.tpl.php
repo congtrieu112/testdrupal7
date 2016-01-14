38e96dfb024611c08b48b66babe187fd21354b31
Gregory <gregory.montigny@digitaslbi.fr>
2016-01-14 16:56:14 +0100

    Fix bug after merge

--- docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
+++ docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
@@ -10,7 +10,7 @@
               <dd>
                 <ul>
                   <li><a href="/<?php print $location; ?>/programmes-immobiliers-neufs-<?php print $object['sanitize_name']; ?>" title="Programmes neufs à <?php print $object['name']; ?>">Programmes neufs <?php print $object['name']; ?></a></li>
-                  <li><a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?>-<?php print $object['numero_departement']; ?>" title="Logements neufs à <?php print $object['name']; ?>">Logements neufs <?php print $object['name']; ?></a></li>
+                  <li><a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?><?php print (!empty($object['numero_departement']) ? '-' . $object['numero_departement'] : ''); ?>" title="Logements neufs à <?php print $object['name']; ?>">Logements neufs <?php print $object['name']; ?></a></li>
                 </ul>
               </dd>
             </dl>
