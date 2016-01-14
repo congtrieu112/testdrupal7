324acd715bbbfcc1e7ad8a8f204f54419f2a8c4f
Gregory <gregory.montigny@digitaslbi.fr>
2016-01-14 13:58:41 +0100

    Remove the 'à' in geolocated sitemap links

--- docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
+++ docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
@@ -4,8 +4,8 @@
       <?php foreach ($results as $object) : ?>
         <li>
           <span><?php print $object['name']; ?></span>
-          <a href="/<?php print $location; ?>/programmes-immobiliers-neufs-<?php print $object['sanitize_name']; ?>" >Programmes neufs à <?php print $object['name']; ?></a>
-          <a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?><?php print (!empty($object['numero_departement']) ? '-' . $object['numero_departement'] : ''); ?>" >Logements neufs à <?php print $object['name']; ?></a>
+          <a href="/<?php print $location; ?>/programmes-immobiliers-neufs-<?php print $object['sanitize_name']; ?>" >Programmes neufs <?php print $object['name']; ?></a>
+          <a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?><?php print (!empty($object['numero_departement']) ? '-' . $object['numero_departement'] : ''); ?>" >Logements neufs <?php print $object['name']; ?></a>
         </li>
       <?php endforeach; ?>
     </ul>
