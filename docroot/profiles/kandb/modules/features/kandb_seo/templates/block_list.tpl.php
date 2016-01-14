14b6ea58be947bbcd2a95b4cfc7e0efbc8207081
Gregory <gregory.montigny@digitaslbi.fr>
2016-01-14 11:21:41 +0100

    Remove code departement for region

--- docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
+++ docroot/profiles/kandb/modules/features/kandb_seo/templates/block_list.tpl.php
@@ -5,7 +5,7 @@
         <li>
           <span><?php print $object['name']; ?></span>
           <a href="/<?php print $location; ?>/programmes-immobiliers-neufs-<?php print $object['sanitize_name']; ?>" >Programmes neufs à <?php print $object['name']; ?></a>
-          <a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?>-<?php print $object['numero_departement']; ?>" >Logements neufs à <?php print $object['name']; ?></a>
+          <a href="/<?php print $location; ?>/logements-immobiliers-neufs-<?php print $object['sanitize_name']; ?><?php print (!empty($object['numero_departement']) ? '-' . $object['numero_departement'] : ''); ?>" >Logements neufs à <?php print $object['name']; ?></a>
         </li>
       <?php endforeach; ?>
     </ul>
