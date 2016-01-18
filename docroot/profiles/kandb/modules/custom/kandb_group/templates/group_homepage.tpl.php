b4dfaa94f9f9d0a95c78a98597f1a56affcc646e
Trang.Tran <trang.tran@sutrixmedia.com>
2016-01-18 17:26:24 +0700

    feature-KTMA-104 revome comment code

--- docroot/profiles/kandb/modules/custom/kandb_group/templates/group_homepage.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_group/templates/group_homepage.tpl.php
@@ -53,7 +53,6 @@ endif;
                                     <li><a href="<?php print url($value['url']); ?>" title="<?php print trim($value['title']); ?>"><?php print trim($value['title']); ?></a></li>
                                   <?php endif; ?>
                                 <?php endforeach; ?>
-                                <!--<li><a href="<?php // print url('corporate/finance/presentation/en'); ?>" title="<?php // print t('English version'); ?>"><?php // print t('English version'); ?></a></li>-->
                               <?php endif; ?>
                           </ul>
                       </div>
