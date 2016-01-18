ae2da833e588347fa20b7d6448ccc98e78a67d96
Trang.Tran <trang.tran@sutrixmedia.com>
2016-01-18 11:38:30 +0700

    feature-KTMA-104 delete 'English version' link and add required for field image

--- docroot/profiles/kandb/modules/custom/kandb_group/templates/group_homepage.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_group/templates/group_homepage.tpl.php
@@ -53,7 +53,7 @@ endif;
                                     <li><a href="<?php print url($value['url']); ?>" title="<?php print trim($value['title']); ?>"><?php print trim($value['title']); ?></a></li>
                                   <?php endif; ?>
                                 <?php endforeach; ?>
-                                <li><a href="<?php print url('corporate/finance/presentation/en'); ?>" title="<?php print t('English version'); ?>"><?php print t('English version'); ?></a></li>
+                                <!--<li><a href="<?php // print url('corporate/finance/presentation/en'); ?>" title="<?php // print t('English version'); ?>"><?php // print t('English version'); ?></a></li>-->
                               <?php endif; ?>
                           </ul>
                       </div>
@@ -155,7 +155,7 @@ endif;
                       </div>
                   </div>
               </div>
-            <?php endif; ?> 
+            <?php endif; ?>
             <!--            <div data-equalizer-watch="data-equalizer-watch" class="articleList__item">
                             <div class="inner">
                                 <div class="articleList__item__img">
