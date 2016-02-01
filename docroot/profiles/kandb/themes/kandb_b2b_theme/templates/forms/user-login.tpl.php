293529d22923ce5f61fbb5832756bd4ea221b096
hau.le <hau.le@sutrixmedia.com>
2016-02-01 17:21:48 +0700

    #KBC-1586 redirect and set default value for homepage

--- docroot/profiles/kandb/themes/kandb_b2b_theme/templates/forms/user-login.tpl.php
+++ docroot/profiles/kandb/themes/kandb_b2b_theme/templates/forms/user-login.tpl.php
@@ -6,12 +6,12 @@
 ?>
 
 <div class="heading heading--switchColor">
-  <h3 class="heading__title">Déjà partenaire ?</h3>
+  <h3 class="heading__title"><?php print t('Déjà partenaire ?'); ?> </h3>
 </div>
 <div class="homepageB2B__login--textfield">
-  <label for="userName" class="visually-hidden">Votre identifiant <span class="form-required" title="This field is required.">*</span></label>
+  <label for="userName" class="visually-hidden"><?php print t('Votre identifiant'); ?> <span class="form-required" title="<?php print t('This field is required.'); ?>">*</span></label>
   <?php print drupal_render($form['name']); ?>
-  <label for="password" class="visually-hidden">Votre mot de passe <span class="form-required" title="This field is required.">*</span></label>
+  <label for="password" class="visually-hidden"><?php print t('Votre mot de passe'); ?> <span class="form-required" title="<?php print t('This field is required.'); ?>">*</span></label>
   <?php print drupal_render($form['pass']); ?>
 </div>
 <div class="homepageB2B__login--inlineGroup">
@@ -21,7 +21,7 @@
       <span><?php print t('Se souvenir de moi'); ?></span>
     </label>
   </div>
-  <a href="#" title="Mot de passe oublié?" class="textLink">
+  <a href="<?php print url('user/password') ?>" title="<?php print t('Mot de passe oublié?'); ?> " class="textLink">
     <?php print t('Mot de passe oublié?'); ?>
   </a>
 </div>
