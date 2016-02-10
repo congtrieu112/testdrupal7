b10e8d2c8568176dca1fe49ae1863d0f12d3f899
Gregory <gregory.montigny@digitaslbi.fr>
2016-02-10 13:03:26 +0100

    KBC-1774 Add Back Button

--- docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-programme.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/nodes/ds-1col--node-programme.tpl.php
@@ -5,6 +5,9 @@ $bon_plan = $node->field_programme_habiteo_bon_plan[LANGUAGE_NONE][0]['value'];
 <header class="programHeader">
     <!-- mobile heading-->
     <div class="wrapper show-for-small-only">
+        <!-- [back link] start -->
+        <!-- the document.referrer page must start with the 'referrerStart' value ('recherche' for example from recherche page)--><a href="#" data-back="{&quot;referrerStart&quot;:&quot;B2C-searchResults&quot;}" class="btn-white hidden">Retour<span class="icon icon-arrow left"></span></a>
+        <!-- [back link] start-->
         <h1 class="heading heading--bordered">
             <?php if ($program_loc_ville) : ?>
               <div class="heading__title">
@@ -82,6 +85,9 @@ $bon_plan = $node->field_programme_habiteo_bon_plan[LANGUAGE_NONE][0]['value'];
         <div class="programHeader__content">
             <div class="toolbox">
                 <!-- tablet+desktop heading-->
+                <!-- [back link] start -->
+                <!-- the document.referrer page must start with the 'referrerStart' value ('recherche' for example from recherche page)--><a href="#" data-back="{&quot;referrerStart&quot;:&quot;B2C-searchResults&quot;}" class="btn-white hidden">Retour<span class="icon icon-arrow left"></span></a>
+                <!-- [back link] start-->
                 <div class="show-for-medium-up">
                     <h1 class="heading heading--bordered">
                         <?php if ($program_loc_ville) : ?>
