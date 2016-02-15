96ed889d11269ebf3836734f0150382b02ad5c95
hau.le <hau.le@sutrixmedia.com>
2016-02-15 16:04:13 +0700

    #KBC-1661 update template view news avant-premiere

--- docroot/profiles/kandb/modules/custom/kandb_b2b/modules/kandb_dashboard/templates/b2b_simple_dash_board.tpl.php
+++ docroot/profiles/kandb/modules/custom/kandb_b2b/modules/kandb_dashboard/templates/b2b_simple_dash_board.tpl.php
@@ -8,21 +8,42 @@ $dash_board_header = module_invoke('bean', 'block_view', 'dash-board');
 if (isset($dash_board_header['content']) AND $dash_board_header['content']) :
   print render($dash_board_header['content']);
 endif;
+$latest_offers = views_embed_view('b2b_nos_dernieres_offres', 'block');
 ?>
-<!-- [dashboardHeading] start-->
-<!-- images need to have 2 different pictures see data-exchange attribute:
-- small: 640 x 580
-- large: 1380 x 580
--->
-<section class="homepageB2B dashboard-b2b">
-    <?php
-    $latest_offers = views_embed_view('b2b_nos_dernieres_offres', 'block');
-    print $latest_offers;
-    ?>
-</section>
-<!-- [dashboardHeading] end-->
+
+<!-- [offers] start-->
+<?php if ($latest_offers): ?>
+  <section class="section-padding">
+      <div class="wrapper">
+          <h2 class="heading--tiny"><?php print t('Les offres spéciales'); ?></h2>
+          <!-- [carousel] start-->
+          <?php
+          print $latest_offers;
+          ?>
+          <!-- [carousel] end-->
+      </div>
+  </section>
+  <!-- [offers] end-->
+<?php endif; ?>
+
+
+
 
 <?php
-$special_offers = views_embed_view('b2b_nos_dernieres_offres', 'block-1');
-print $special_offers;
-?>
+$special_offers = views_embed_view('b2b_nos_dernieres_offres', 'block_2');
+if ($special_offers):
+  ?>
+  <!-- [nouvates] start-->
+  <section class="section-padding bg-lightGrey">
+      <div class="wrapper">
+          <h2 class="heading--tiny"><?php print t('Nouveautés et avant premières'); ?></h2>
+          <!-- [carousel] start-->
+          <?php
+          print $special_offers;
+          ?>
+          <!-- [carousel] end-->
+      </div>
+  </section>
+  <!-- [nouvates] end-->
+
+<?php endif; ?>
