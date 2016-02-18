1bdb6a202fe3b62d4544f752b09d28ae8f7e5c55
Gregory <gregory.montigny@digitaslbi.fr>
2016-02-18 13:35:03 +0100

    KBC-1887 Adding Geolocalisation to Home page view

--- docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-unformatted--nos-dernieres-offres--block.tpl.php
+++ docroot/profiles/kandb/themes/kandb_theme/templates/views/views-view-unformatted--nos-dernieres-offres--block.tpl.php
@@ -8,10 +8,13 @@
 ?>
 
 <!-- [carousel] start-->
-<div data-slick="{&quot;slidesToShow&quot;: 3, &quot;slidesToScroll&quot;: 3}" class="slick-slider__item-3">
-    <?php $_SESSION['promotion'] = 0; ?>
-    <?php foreach ($rows as $id => $row): ?>
-      <?php print $row; ?>
-    <?php endforeach; ?>
+<div class="geoloc-wait" data-geoloc-target="<?php print $view->name; ?>" data-geoloc-target-args="module:views;display:<?php print $view->current_display; ?>;" >
+  <?php if(!IS_AJAX): ?><div class="geoloc-wait__spinner" ></div><?php endif; ?>
+  <div data-slick="{&quot;slidesToShow&quot;: 3, &quot;slidesToScroll&quot;: 3}" class="slick-slider__item-3">
+      <?php $_SESSION['promotion'] = 0; ?>
+      <?php foreach ($rows as $id => $row): ?>
+        <?php print $row; ?>
+      <?php endforeach; ?>
+  </div>
 </div>
 <!-- [carousel] end-->
